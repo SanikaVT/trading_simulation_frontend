52a12ddf32ecea12a567d5c8da8a883aceec2f45
sureja <dh276903@dal.ca>
2022-06-14 00:38:17 -0300

    Add Feature Blog

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -2,40 +2,29 @@ import * as React from 'react';
 import BlogCard from "./BlogCard";
 import Grid from '@mui/material/Grid';
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import {ButtonBase} from "@mui/material";
 import AddIcon from '@mui/icons-material/Create';
-
 import Button from "@mui/material/Button";
-import {auto} from "framer-motion/types/render/dom/value-types/type-auto";
-import Typography from "@mui/material/Typography";
 export default function Blog () {
 
-    function  hello () {
-        console.log("hello ji");
+    function  write () {
+        console.log("write blog");
+        window.location.href="/writeblog";
     }
 
-const icon1 ={
-        height:100,
-        width:100,
-        color:"#2E8BC0"
-    // display: 'flex',
-    // alignItems: 'center',
 
-};
 
-    const card_1 = {
-        backgroundColor: "white",
-        borderRadius: "10px",
-        borderWidth: 1,
-         // margin: "10px",
-        // height: "full",
-        padding: "10px",
-        // size: 100
+    const buttonprop ={
+         top: 5,
+      bgcolor: "#2E8BC0",
+        float: 'right',
+        borderRadius: '15px'
+
     };
+
     return(
-        // <Grid><BlogCard/></Grid>
+
 <Box sx={{ flexGrow: 1 }}>
+    <Button variant="contained" endIcon={<AddIcon />} sx={buttonprop} onClick={write}> Write blog</Button>
     <Grid container spacing={2} columns={{ xs: 4, sm: 8, md: 12 }}>
 
         <BlogCard/>
@@ -44,23 +33,10 @@ const icon1 ={
         <BlogCard/>
         <BlogCard/>
         <BlogCard/>
-        <Grid item xs={6} md={3}>
-            <ButtonBase onClick={hello}>
 
-            <Card variant="outlined" style={card_1} sx={{ height: 500, maxWidth: 320}}>
-
-                   <Grid container spacing={25} alignItems={"center"} justifyContent={"center"} direction={"row"}>
-                   <Grid item xs={12} md={12} >
-                    <AddIcon style={icon1} />
-                   </Grid>
-                       <Grid item xs={12} md={12} >
-                          <Typography variant="h4"  > Add Blog</Typography>
-                       </Grid>
-                   </Grid>
-            </Card>
-        </ButtonBase>
-        </Grid>
     </Grid>
+
+
 </Box>
     );
              }
