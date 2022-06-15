04c76a7964dbe6a6742844f52df0b8b925f17a04
Udit Gandhi <coder.udit@gmail.com>
2022-06-15 10:40:20 -0300

    Updated with orders in header component.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,10 +6,19 @@ import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 import { useNavigate } from "react-router-dom";
 import Tooltip from "@mui/material/Tooltip";
-
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import PersonIcon from "@mui/icons-material/Person";
 
 function Header() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   let navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -29,10 +38,37 @@ function Header() {
             <PersonIcon className="profile"></PersonIcon>
           </Tooltip>
 
-          <Tooltip title="Order Status" arrow>
-            <Button className="header" onClick={() => navigate("/orderstatus")}>
-              Orders
-            </Button>
+          <Tooltip title="Portfolio" arrow>
+            <>
+              <Button
+                className="header"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                Orders
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem onClick={handleClose}>Positions</MenuItem>
+                <MenuItem onClick={handleClose}>Holdings</MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    navigate("/orderstatus");
+                  }}
+                >
+                  Orders
+                </MenuItem>
+              </Menu>
+            </>
           </Tooltip>
 
           <Tooltip title="Technical Support" arrow>
