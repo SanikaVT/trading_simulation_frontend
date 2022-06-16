54dd1da7e0b1d20b483dced0de2320f0cc32f7de
Sanika Tamhankar <sn295037@dal.ca>
2022-06-16 15:59:40 -0300

    Removed footer

--- src/components/footer/FooterComp.tsx
+++ src/components/footer/FooterComp.tsx
@@ -1,37 +1,8 @@
 import { Box, Grid } from "@mui/material";
-import React from "react";
-import FooterLinksComp from "./FooterInfo";
-import { Divider } from "@mui/material";
 
 function FooterComp() {
   return (
     <Box sx={{ mt: 5, flexGrow: 1, bgcolor: "#2E8BC0" }}>
-      {/* <Grid container spacing={1} sx={{ pt: 4 }}>
-        <Grid item xs={4}>
-          <Box textAlign="center" fontWeight="bold" color="white" mb={1}>
-            About
-          </Box>
-          <FooterLinksComp name="Our Company" />
-          <FooterLinksComp name="Careers" />
-          <FooterLinksComp name="Social" />
-        </Grid>
-        <Grid item xs={4}>
-          <Box textAlign="center" fontWeight="bold" color="white" mb={1}>
-            Portfolio
-          </Box>
-          <FooterLinksComp name="Positions" />
-          <FooterLinksComp name="Holdings" />
-          <FooterLinksComp name="Order Status" link="orderstatus" />
-        </Grid>
-        <Grid item xs={4}>
-          <Box textAlign="center" fontWeight="bold" color="white" mb={1}>
-            Terms & Policies
-          </Box>
-          <FooterLinksComp name="Terms & Conditions" />
-          <FooterLinksComp name="Privacy" />
-          <FooterLinksComp name="Cookies" />
-        </Grid>
-      </Grid> */}
       <Grid container color="white" direction="column">
         <Grid item xs={12} textAlign="center" sx={{ mt: 3, mb: 3 }}>
           © 2016 - 2022 DTrade.com. All rights reserved.
