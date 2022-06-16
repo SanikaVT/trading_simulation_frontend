573e31c32c046586c2d781a5ce7e280c678923ed
sureja <dh276903@dal.ca>
2022-06-16 18:24:10 -0300

    small changes

--- src/components/blog/BlogDetails.tsx
+++ src/components/blog/BlogDetails.tsx
@@ -12,8 +12,6 @@ import CloseIcon from '@mui/icons-material/Close';
 import Divider from '@mui/material/Divider';
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogActions from "@mui/material/DialogActions";
 
 export default function BlogDetails() {
