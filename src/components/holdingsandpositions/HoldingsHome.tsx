e052c08ec9241a60d08fee12c46874cc06a4bd5d
Udit Gandhi <coder.udit@gmail.com>
2022-07-14 11:54:30 -0300

    Delete wrong paths from holdings

--- src/components/holdingsandpositions/HoldingsHome.tsx
+++ src/components/holdingsandpositions/HoldingsHome.tsx
@@ -1,48 +1,53 @@
-import PortfolioTable from './PortfolioTable'
-import Navbar from './Navbar'
+import PortfolioTable from "./PortfolioTable";
 import Button from "@mui/material/Button";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import React from "react";
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 
 export default function HoldingsHome() {
-    const navigate = useNavigate();
-    const routeChange = () =>{
-        let path = `/report`;
-        navigate(path);
-    }
+  const navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/report`;
+    navigate(path);
+  };
 
-    const routeChangeHome = () =>{
-        let path = `/holdings`;
-        navigate(path);
-    }
+  const routeChangeHome = () => {
+    let path = `/holdings`;
+    navigate(path);
+  };
 
-    const routeChangePosi = () =>{
-        let path = `/positions`;
-        navigate(path);
-    }
+  const routeChangePosi = () => {
+    let path = `/positions`;
+    navigate(path);
+  };
 
-    return (
-
-        <div>
-            <br/>
-            <Grid container direction="row" alignItems="center"
-                  justifyContent="center" spacing={2} >
-                <Grid item>
-                    <Button variant="contained" onClick={routeChangeHome}>Holdings</Button>
-                </Grid>
-                <Grid item>
-                    <Button variant="outlined" onClick={routeChangePosi}>Positions</Button>
-                </Grid>
-                <Grid item>
-                    <Button variant="outlined" onClick={routeChange}>Report</Button>
-                </Grid>
-            </Grid>
-            <PortfolioTable/>
-
-
-        </div>
-    );
+  return (
+    <div>
+      <br />
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={2}
+      >
+        <Grid item>
+          <Button variant="contained" onClick={routeChangeHome}>
+            Holdings
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" onClick={routeChangePosi}>
+            Positions
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" onClick={routeChange}>
+            Report
+          </Button>
+        </Grid>
+      </Grid>
+      <PortfolioTable />
+    </div>
+  );
 }
-
-
