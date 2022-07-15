bcb82de7e6dfcdfa680bcdfbfa841087df227af1
Sanika Tamhankar <sn295037@dal.ca>
2022-07-15 17:39:13 -0300

    Update PieChartComp.tsx

--- src/components/forum/PieChartComp.tsx
+++ src/components/forum/PieChartComp.tsx
@@ -1,3 +1,8 @@
+/**
+ * Author: Sanika Tamhankar
+ * BannerID: B00909848
+ * Email: sn295037@dal.ca
+ */
 import { Pie } from "react-chartjs-2";
 import "chart.js/auto";
 import { useEffect, useState } from "react";
