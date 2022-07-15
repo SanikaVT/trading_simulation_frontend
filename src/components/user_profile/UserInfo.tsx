a6f75ad781cb1cf5ef53a0f6bcc33d4c61d6645d
Sanika Tamhankar <sn295037@dal.ca>
2022-07-15 17:42:03 -0300

    Update UserInfo.tsx

--- src/components/user_profile/UserInfo.tsx
+++ src/components/user_profile/UserInfo.tsx
@@ -1,3 +1,8 @@
+/**
+ * Author: Sanika Tamhankar
+ * BannerID: B00909848
+ * Email: sn295037@dal.ca
+ */
 import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import { Typography } from "@mui/material";
