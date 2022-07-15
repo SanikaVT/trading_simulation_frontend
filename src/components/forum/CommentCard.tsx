4636e309b66af0b5a16b6498013cbb92376d5582
Sanika Tamhankar <sn295037@dal.ca>
2022-07-15 17:38:32 -0300

    Update CommentCard.tsx

--- src/components/forum/CommentCard.tsx
+++ src/components/forum/CommentCard.tsx
@@ -1,3 +1,8 @@
+/**
+ * Author: Sanika Tamhankar
+ * BannerID: B00909848
+ * Email: sn295037@dal.ca
+ */
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
